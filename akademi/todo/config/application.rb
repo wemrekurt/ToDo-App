101234cae3a49405135d04369316fc3da741bc7b
wemrekurt <emre.kurt@bil.omu.edu.tr>
2016-07-18 23:39:58 +0300

    tamamlandı

--- akademi/todo/config/application.rb
+++ akademi/todo/config/application.rb
@@ -19,7 +19,12 @@ module Todo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :put, :delete, :patch, :post, :options]
+      end
+    end
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
--- todo.js
+++ todo.js
@@ -1,30 +1,83 @@
 // DRY
 var app = angular.module('ToDo',['ngRoute']);
 
-app.controller("TodoCtrl",function($scope){
+app.controller("TodoCtrl",function($scope, $http){
   $scope.todonew="";
-  $scope.todos = JSON.parse(localStorage.getItem("Todos")) || {0: {text:'Evi Temizle', state:false} };
 
+  /*
+  * Todo elemanları rails tarafından çağrılarak scopa'a eklenir.
+  */
+  $http.get("http://localhost:3000/jobs.json")
+    .then(function(response) {
+        $scope.todos = response.data;
+    });
+
+  /**
+  * TODDO elemanını railse post ile gönder ve scope'a ekle
+  */
   $scope.setTodo = function(){
     if($scope.todonew) {
-      $scope.todos[Object.keys($scope.todos).length] = {text:$scope.todonew, state:false};
-      $scope.setScope();
+
+      $http({
+        method: 'POST',
+        url: 'http://localhost:3000/jobs.json',
+        data: {title: $scope.todonew, state: false}
+      }).then(function successCallback(response) {
+            if(response.data.id){
+              // $scope.todos[$scope.getKey()] = {id:response.data.id, title:response.data.title, state:false};
+              $scope.todos.push({id:response.data.id, title:response.data.title, state:false});
+            }else{
+              alert('Error! Rails problem');
+            }
+        }, function errorCallback(response) {
+            alert("Error!");
+        });
       $scope.todonew = '';
     }
   }
 
+  /**
+  * TODDO elemanını railste siler ve scopedaki o keye ait itemı siler. o item undefined olur.
+  * Örneğin delete işlemi yapıldığında console.log($scope.todos[key]) çıktısı undefined olur.
+  */
   $scope.clear = function(){
     angular.forEach($scope.todos, function(value, key) {
-        if (value.state) delete $scope.todos[key];
+        if (value.state){
+          $http({
+            method: 'DELETE',
+            url: 'http://localhost:3000/jobs/'+ value.id +'.json',
+            data: {id: value.id}
+          }).then(function successCallback(response){
+               delete $scope.todos[key];
+            }
+          )
+        }
     });
-    $scope.setScope();
   }
 
-  $scope.setScope = function(){
-    localStorage.setItem('Todos', JSON.stringify($scope.todos));
+  /**
+  * TODDO'da elemanı günceller. Checkbox tıklandığında scope zaten güncellendiği için sadece rails'e istek gönderilir.
+  */
+  $scope.setScope = function(key){
+    $http({
+      method: 'PATCH',
+      url: 'http://localhost:3000/jobs/'+ $scope.todos[key].id +'.json',
+      data: $scope.todos[key]
+    }).then(function successCallback(response) {
+          if(!response.data.id){
+            alert('Error! Rails problem');
+          }
+      }, function errorCallback(response) {
+          alert("Error!");
+      });
+
   }
 
 })
+
+/**
+* Rota Ayarları
+*/
 
 app.config(function($routeProvider) {
     $routeProvider
